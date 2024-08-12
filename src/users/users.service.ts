c3e4818a8da67f8f1914a71dcb40b98b6d8ef75a
PedroFranca2299 <github.reactive686@passmail.net>
2024-08-12 11:42:10 -0300

    implement basic crud functionality

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,79 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      "id": 1,
+      "name": "Pedro",
+      "email": "pedro.cornutti@topinvest.com.br",
+      "role": "INTERN"
+    },
+    {
+      "id": 2,
+      "name": "Opedr",
+      "email": "opedr.cornutti@topinvest.com.br",
+      "role": "INTERN"
+    },
+    {
+      "id": 3,
+      "name": "Roped",
+      "email": "roped.cornutti@topinvest.com.br",
+      "role": "ENGINEER"
+    },
+    {
+      "id": 4,
+      "name": "Drope",
+      "email": "drope.cornutti@topinvest.com.br",
+      "role": "ENGINEER"
+    },
+    {
+      "id": 5,
+      "name": "Edrop",
+      "email": "edrop.cornutti@topinvest.com.br",
+      "role": "ADMIN"
+    }
+  ]
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter(user => user.role === role)
+    }
+    return this.users
+  }
+
+  findOne(id: number) {
+    const user = this.users.find(user => user.id === id)
+    return user
+  }
+
+  create(user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+    const userByHighestId = [...this.users].sort((a, b) => b.id - a.id)
+    const newUser = {
+      id: userByHighestId[0].id + 1,
+      ...user
+    }
+    this.users.push(newUser)
+    return newUser
+  }
+
+  update(id: number, updatedUser: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+    this.users = this.users.map(user => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser }
+      }
+      return user
+    })
+
+    return this.findOne(id)
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id)
+
+    this.users = this.users.filter(user => user.id !== id)
+
+    return removedUser
+  }
+
+}
